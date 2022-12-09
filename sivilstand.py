fa4a5ab784a7c059e82c16fc5991a5d76a8a8f7a
jennypenny04 <e-jengri@iskule.no>
2022-12-09 09:03:59 +0100

    Lagt inn graf for befolkning

--- sivilstand.py
+++ sivilstand.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import json
+import csv
 
 
 class Fil:
@@ -7,6 +8,8 @@ class Fil:
         self.filnavn = filnavn
         self.filtype = filtype
         self.overskrift = overskrift
+        self.aarstall = []
+        self.utvikling = []
 
         if filtype == "json":
             with open(self.filnavn, encoding="utf-8-sig") as fil:
@@ -23,6 +26,20 @@ class Fil:
                 self.label = data["dataset"]["dimension"]["EkteskStatus"]["category"]["label"]
                 self.label = list(self.label.values())
 
+        elif filtype == "csv":
+            with open(self.filnavn, encoding="utf-8-sig") as fil:
+                filinnhold = csv.reader(fil, delimiter=";")
+
+                overskrifter = next(filinnhold)
+                next(filinnhold)
+                next(filinnhold)
+                print(overskrifter)
+
+                for rad in filinnhold:
+                    print(rad)
+                    self.aarstall.append(int(rad[0])) #x-akse
+                    self.utvikling.append(int(rad[1])) #y-akse
+
     def PlassereData(self):
         self.utvikling = []
 
@@ -35,11 +52,15 @@ class Fil:
         print(self.utvikling)
 
     def plotteGrafer(self):
-        for i in range(0, 5):
-            plt.plot(self.aarstall, self.utvikling[i], label=self.label[i])
+        if self.filtype == "json":
+            for i in range(0, 5):
+                plt.plot(self.aarstall, self.utvikling[i], label=self.label[i])
+        elif self.filtype == "csv":
+            plt.plot(self.aarstall, self.utvikling)  # Lager stolpediagrammet
+
 
     def pynteGrafer(self):
-        plt.ylim(0, 4000000)
+        plt.ylim(0, 5500000)
         plt.xlim(1769, 2022)
         plt.xlabel("")
         plt.ylabel("")
@@ -48,9 +69,15 @@ class Fil:
         plt.legend()
         plt.show()
 
-
+"""
 sivilstand = Fil("Sivilstand.json", "json", "Sivilstand fra 1769 til i dag")
 sivilstand.PlassereData()
 # sivilstand.skriveLister()
 sivilstand.plotteGrafer()
 sivilstand.pynteGrafer()
+"""
+
+befolkning = Fil("Befolkning.csv","csv","Befolkning i Norge per 1. januar 1769-2022")
+befolkning.plotteGrafer()
+befolkning.pynteGrafer()
+
