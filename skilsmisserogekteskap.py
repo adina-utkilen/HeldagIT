88838ec6f2bdcdfad86fe64f1475bc9828f4250f
adina-utkilen <e-adiutk@asvg.no>
2022-12-09 10:24:50 +0100

    endringer på søylediagram

--- skilsmisserogekteskap.py
+++ skilsmisserogekteskap.py
@@ -1,5 +1,6 @@
 import csv
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 class Fil:
@@ -8,9 +9,6 @@ class Fil:
         self.filtype = filtype
         self.overskrift = overskrift
         self.data = []
-        self.aarstall = []
-        self.ekteskap = []
-        self.skillsmisse = []
 
         if filtype == "csv":
             with open(filnavn, encoding="utf-8") as fil:
@@ -22,13 +20,40 @@ class Fil:
                 for rad in self.filinnhold:
                     self.data.append(rad)
 
-                self.aarstall.append(self.data[0])
-                self.ekteskap.append(self.data[1])
-                self.skillsmisse.append(self.data[2])
+                for i in range(0, len(self.data)):
+                    self.data[i].pop(0)
 
-    def fjerneOverskrift(self):
-        self.aarstall.pop()
+                self.aarstall = self.data[0]
+                self.ekteskap = self.data[1]
+                self.skillsmisse = self.data[2]
+
+                for i in range(0, len(self.aarstall)):
+                    self.aarstall[i] = int(self.aarstall[i])
+
+                for i in range(0, len(self.ekteskap)):
+                    self.ekteskap[i] = int(self.ekteskap[i])
+
+                for i in range(0, len(self.skillsmisse)):
+                    self.skillsmisse[i] = int(self.skillsmisse[i])
+
+    def plotteGraf(self):
+        fig, ax = plt.subplots(figsize=(10, 5))
+        y = np.arange(10)
+        # Lager stolpediagram jenter
+        ax.barh(y+0.2, self.ekteskap, height=0.4, label="Jenter")
+        # Lager stolpediagram gutter
+        ax.barh(y-0.2, self.skillsmisse, height=0.4, label="Gutter")
+        # Legger til akseverdier
+        ax.set_yticks(y, self.aarstall)
+        ax.legend()                                               # Legger til beskrivelse
+
+        # Øker plassen på venstre side av diagrammet
+        fig.subplots_adjust(left=0.4)
+
+        ax.grid(axis="x")  # Legger til rutenett (bare vertikale linjer)
+        plt.show()
 
 
 Skilsmisser = Fil("Skilsmisserogekteskap.csv", "csv",
                   "Inngåtte ekteskap og skilsmisser fra 1902 til 2022")
+Skilsmisser.plotteGraf()
