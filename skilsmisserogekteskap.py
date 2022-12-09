e8efb6d865d6c448cc2f5868520809706b55a706
adina-utkilen <e-adiutk@asvg.no>
2022-12-09 09:52:19 +0100

    en start på søylediagram

---
+++ skilsmisserogekteskap.py
@@ -0,0 +1,33 @@
+import csv
+import matplotlib.pyplot as plt
+
+
+class Fil:
+    def __init__(self, filnavn, filtype, overskrift):
+        self.filnavn = filnavn
+        self.filtype = filtype
+        self.overskrift = overskrift
+        self.data = []
+        self.aarstall = []
+        self.ekteskap = []
+        self.skillsmisse = []
+
+        if filtype == "csv":
+            with open(filnavn, encoding="utf-8") as fil:
+                self.filinnhold = csv.reader(fil, delimiter=";")
+
+                overskrifter = next(self.filinnhold)
+              #  print(str(overskrifter))
+
+                for rad in self.filinnhold:
+                    self.data.append(rad)
+
+                self.aarstall.append(self.data[0])
+                self.ekteskap.append(self.data[1])
+                self.skillsmisse.append(self.data[2])
+
+                print(self.aarstall)
+
+
+Skilsmisser = Fil("Skilsmisserogekteskap.csv", "csv",
+                  "Inngåtte ekteskap og skilsmisser fra 1902 til 2022")
